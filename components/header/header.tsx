d384cac09a9e843bce1cd9b83824e160c2c1d30c
Dexter096 <gr1nch2@yandex.ru>
2023-08-05 09:01:11 +0300

    fix: add auth folder

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Header = (): ReactElement => {
   return (
     <header>
-      <Link href="/registration">Registration Page </Link>
-      <Link href="/login">Login Page</Link>
+      <Link href="/auth/registration">Registration Page </Link>
+      <Link href="/auth/login">Login Page</Link>
     </header>
   );
 };
