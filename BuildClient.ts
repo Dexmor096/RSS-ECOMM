17a2993c714196163da57749c37806c593927f62
Inna T <meeoow005@gmail.com>
2023-08-12 21:19:34 +0200

    feat: anonymous authorization

---
+++ BuildClient.ts
@@ -0,0 +1,47 @@
+import fetch from "node-fetch";
+import {
+  ClientBuilder,
+
+  // Import middlewares
+  type AuthMiddlewareOptions, // Required for auth
+  type HttpMiddlewareOptions, // Required for sending HTTP requests
+} from "@commercetools/sdk-client-v2";
+
+export const projectKey = "ygvyvt";
+const scopeList = [
+  `view_published_products`,
+  `create_anonymous_token`,
+  `view_categories`,
+  `manage_my_profile`,
+  `manage_my_payments`,
+  `manage_my_business_units`,
+  `manage_my_shopping_lists`,
+  `manage_my_orders`,
+];
+const scopes = scopeList.map((it) => it.concat(":", projectKey));
+
+// Configure authMiddlewareOptions
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: "https://auth.europe-west1.gcp.commercetools.com",
+  projectKey: projectKey,
+  credentials: {
+    clientId: "1ppOtIM0a14j4IeryfUtXNh4",
+    clientSecret: "z7jAsa5JLN8Yz8AWRfkCgzKgEWqMegyc",
+  },
+  scopes,
+  fetch,
+};
+
+// Configure httpMiddlewareOptions
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: "https://api.europe-west1.gcp.commercetools.com",
+  fetch,
+};
+
+// Export the ClientBuilder
+export const ctpClient = new ClientBuilder()
+  .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withLoggerMiddleware() // Include middleware for logging
+  .build();
