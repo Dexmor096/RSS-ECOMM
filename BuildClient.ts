045d2ff93372e01782b601555d83e6957b8a57dd
Inna T <meeoow005@gmail.com>
2023-08-21 01:37:16 +0200

    feat: create environment variables, refactor ctp anon client func, add ctp for auth

--- BuildClient.ts
+++ BuildClient.ts
@@ -7,8 +7,10 @@ import {
   AnonymousAuthMiddlewareOptions, // Required for sending HTTP requests
 } from "@commercetools/sdk-client-v2";
 import { generateRandomNumber } from "./helpers/generateNumber";
+import { PasswordAuthMiddlewareOptions } from "@commercetools/sdk-client-v2/dist/declarations/src/types/sdk";
+import * as process from "process";
 
-export const projectKey = "ygvyvt";
+export const projectKey = process.env.CTP_PROJECT_KEY!;
 const scopeList = [
   `view_published_products`,
   `create_anonymous_token`,
@@ -19,6 +21,7 @@ const scopeList = [
   `manage_my_shopping_lists`,
   `manage_my_orders`,
   `manage_customers`,
+  //todo создать новый клиент без `manage_customers`
 ];
 const scopes = scopeList.map((it) => it.concat(":", projectKey));
 
@@ -30,29 +33,45 @@ const httpMiddlewareOptions: HttpMiddlewareOptions = {
 
 const anonymousId = String(generateRandomNumber());
 
-const anonymousOptions: AnonymousAuthMiddlewareOptions = {
-  host: "https://auth.europe-west1.gcp.commercetools.com",
-  projectKey: projectKey,
-  credentials: {
-    clientId: "DbX0DhQieIwQMMmeY-mWhyCu",
-    clientSecret: "A4bf_ce4Vd8EEWuLVxTM-n_4ORo8KATS",
-    anonymousId: anonymousId, // a unique id
-  },
-  scopes,
-  fetch,
+export const getAnonymousClient = () => {
+  const anonymousOptions: AnonymousAuthMiddlewareOptions = {
+    host: "https://auth.europe-west1.gcp.commercetools.com",
+    projectKey: projectKey,
+    credentials: {
+      clientId: process.env.CTP_CLIENT_ID!,
+      clientSecret: process.env.CTP_CLIENT_SECRET!,
+      anonymousId: anonymousId, // a unique id
+    },
+    scopes,
+    fetch,
+  };
+
+  return new ClientBuilder()
+    .withProjectKey(projectKey)
+    .withAnonymousSessionFlow(anonymousOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .withLoggerMiddleware()
+    .build();
 };
 
-// Export the ClientBuilder
-export const ctpAnonymousClient = new ClientBuilder()
-  .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
-  .withAnonymousSessionFlow(anonymousOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware() // Include middleware for logging
-  .build();
+export const getAuthClient = (login: string, password: string) => {
+  const passwordOptions: PasswordAuthMiddlewareOptions = {
+    host: "https://auth.europe-west1.gcp.commercetools.com",
+    projectKey: projectKey,
+    credentials: {
+      clientId: "DbX0DhQieIwQMMmeY-mWhyCu",
+      clientSecret: "A4bf_ce4Vd8EEWuLVxTM-n_4ORo8KATS",
+      user: {
+        username: login,
+        password: password,
+      },
+    },
+  };
 
-// export const ctpClient = new ClientBuilder()
-//   .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
-//   .withClientCredentialsFlow(authMiddlewareOptions)
-//   .withHttpMiddleware(httpMiddlewareOptions)
-//   .withLoggerMiddleware() // Include middleware for logging
-//   .build();
+  return new ClientBuilder()
+    .withProjectKey(projectKey)
+    .withPasswordFlow(passwordOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .withLoggerMiddleware()
+    .build();
+};
