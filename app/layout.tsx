cfbd5c9d4d32c8bca363fda1fd3d331db83d669c
Semen Tretyakov <tretakovs295@gmail.com>
2023-08-23 14:24:10 +0700

    fix: chenge paths import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-<<<<<<< HEAD
-import Header from "../components/header/header";
-import Footer from "components/footer/footer";
-=======
-// import Header from "../components/header";
-// import Footer from "components/footer";
->>>>>>> eda4a4eb29d40ec6e321b3608d7d59a225453f8e
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,11 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* <Header /> */}
-        {children}
-        {/* <Footer /> */}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
