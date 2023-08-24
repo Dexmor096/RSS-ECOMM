bc9a8d45994bccf840b90501ee780ea337d8b089
Dexter096 <gr1nch2@yandex.ru>
2023-08-24 21:45:58 +0300

    chore: add catalog page template

---
+++ app/catalog/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "components/footer";
+import Header from "components/header";
+
+export default function Catalog() {
+  return (
+    <>
+      <Header />
+      <h1>Catalog</h1>
+      <Footer />
+    </>
+  );
+}
