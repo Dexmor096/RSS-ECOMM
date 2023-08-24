6a06967161327589b5cdae900073f58a1b02cc0c
Dexter096 <gr1nch2@yandex.ru>
2023-08-24 22:43:12 +0300

    feat: card view process

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,11 +1,51 @@
-import Footer from "components/footer";
 import Header from "components/header";
+import Footer from "components/footer";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export default function Catalog() {
   return (
     <>
       <Header />
-      <h1>Catalog</h1>
+      <Box>
+        <Container maxWidth="xl">
+          <Stack>
+            <span>breadcrumbs</span>
+            <Typography variant="h2" color="initial">
+              Каталог specialized
+            </Typography>
+            <Box>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="auto"
+                    image="./assets/card-img/mountain.png"
+                    alt=""
+                    sx={{
+                      p: 2,
+                    }}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="body1" component="h5">
+                      Mountain
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      132 товара
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Box>
+          </Stack>
+        </Container>
+      </Box>
       <Footer />
     </>
   );
