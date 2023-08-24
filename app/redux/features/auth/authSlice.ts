9d82587b7e7f5ccd996445b5e3ba16030b981025
Dexter096 <gr1nch2@yandex.ru>
2023-08-24 19:54:10 +0300

    feat: add basic redux structure

---
+++ app/redux/features/auth/authSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    token: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: {},
+});
