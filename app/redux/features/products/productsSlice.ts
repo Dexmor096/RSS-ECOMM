f60ce7face1eb4bccb297f3724db2c33883c418f
Dexter096 <gr1nch2@yandex.ru>
2023-09-03 17:40:55 +0300

    fix: response types conflict

---
+++ app/redux/features/products/productsSlice.ts
@@ -0,0 +1,59 @@
+import { ProductProjection } from "@commercetools/platform-sdk";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiRoot } from "apiRoot";
+
+interface IProductState {
+  status: string;
+  error: string | null;
+  list: ProductProjection[];
+}
+
+const initialState: IProductState = {
+  status: "not loaded",
+  error: null,
+  list: [],
+};
+
+export const loadProducts = createAsyncThunk(
+  "@@products/load-products",
+  async () => {
+    return (
+      apiRoot
+        .productProjections()
+        // .search()
+        .get({
+          queryArgs: { limit: 13, published: true },
+        })
+        .execute()
+        .then((data) => {
+          console.log("Products received", data.body.results);
+          return data.body.results;
+        })
+        .catch((error) => {
+          console.error("error");
+          throw error;
+        })
+    );
+  },
+);
+
+export const productSlice = createSlice({
+  name: "@@products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadProducts.pending, (state) => {
+        state.status = "loading products";
+        state.error = null;
+      })
+      .addCase(loadProducts.rejected, (state, action) => {
+        state.status = "rejected";
+        state.error = "action.payload.error" || action.meta;
+      })
+      .addCase(loadProducts.fulfilled, (state, action) => {
+        state.status = "products received";
+        state.list = action.payload;
+      });
+  },
+});
