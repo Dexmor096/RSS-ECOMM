8625d49fe292315f7d5e4dd56d1548b1132f2018
Inna T <meeoow005@gmail.com>
2023-09-05 20:38:21 +0200

    feat: profile page title

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,13 +3,39 @@ import Header from "../../components/header";
 import * as React from "react";
 import Footer from "../../components/footer";
 import { getMyInfo } from "../controllers/controller";
+import { Container, Typography } from "@mui/material";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 export default function Profile() {
   getMyInfo();
   return (
     <>
       <Header />
-      <h1>Profile</h1>
+      <Container
+        disableGutters={true}
+        sx={{
+          maxWidth: "1220px",
+          height: "auto",
+          alignItems: "center",
+          justifyContent: "start",
+          p: "40px 0",
+        }}
+      >
+        <Typography
+          variant="h1"
+          className={montserrat.className}
+          textTransform="uppercase"
+          fontSize={32}
+          letterSpacing={4}
+        >
+          Личный кабинет
+        </Typography>
+      </Container>
       <Footer />
     </>
   );
