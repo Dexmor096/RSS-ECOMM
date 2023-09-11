9f64fb9a015ce56fd32b8ffae1e46feec66d77a4
Inna T <meeoow005@gmail.com>
2023-09-11 19:22:39 +0200

    fix: delete testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 import Header from "components/header/header";
 import Footer from "components/footer/footer";
 import HeroSection from "components/HeroSection";
-import Button from "@mui/material/Button";
-import { getApiRootWithRefreshToken } from "../apiRoot";
 
 export default function Home() {
   //todo вывод товаров после запроса токена анонима
@@ -12,20 +10,6 @@ export default function Home() {
       <Header />
       <main>
         <HeroSection />
-        <h1>Home Page</h1>
-        <Button
-          onClick={() => {
-            const token = localStorage.getItem("refresh-token");
-            if (!token) {
-              throw new Error("No token");
-            }
-            const apiRoot = getApiRootWithRefreshToken(token);
-            const customerLink = apiRoot.me().get().execute();
-            console.log(customerLink);
-          }}
-        >
-          me
-        </Button>
       </main>
       <Footer />
     </>
