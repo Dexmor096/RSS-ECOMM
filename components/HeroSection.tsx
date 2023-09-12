09149d97a11f8ba171c1e0c59da9c58fb8cebb6a
Semen Tretyakov <tretakovs295@gmail.com>
2023-09-12 20:11:51 +0500

    fix: fix png on webp in banner

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,49 +1,49 @@
-import { Stack, Typography } from "@mui/material";
-import localFont from "next/font/local";
-import headerBg from "../public/assets/images/header-bg.png";
-import ButtonUi from "./UI/ButtonUi";
-import Filter from "./filter/Filter";
+import { Stack, Typography } from '@mui/material';
+import localFont from 'next/font/local';
+import headerBg from '../public/assets/images/header-bg.webp';
+import ButtonUi from './UI/ButtonUi';
+import Filter from './filter/Filter';
 
-const colorArr = ["🟢", "🔵", "🔴", "⚫"];
+const colorArr = ['🟢', '🔵', '🔴', '⚫'];
 
 const proheroFont = localFont({
-  src: "../public/assets/fonts/prohero.ttf",
+	src: '../public/assets/fonts/prohero.ttf',
 });
 
 function HeroSection() {
-  return (
-    <>
-      <Stack
-        component="section"
-        direction="column"
-        alignItems="center"
-        sx={{
-          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.45) 100%), url(${headerBg.src}), lightgray 50% / cover no-repeat`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <Typography
-          pt="127px"
-          variant="h1"
-          className={proheroFont.className}
-          color="#fff"
-          fontSize={55}
-          letterSpacing={5.5}
-          width={560}
-        >
-          Ride bikes. Have fun. Feel good.
-        </Typography>
-        <ButtonUi
-          margin="65px 0 285px"
-          text="Найди свой велосипед"
-          color="#fff"
-          border="2px solid #fff"
-        />
-      </Stack>
-      <Filter nameFilter="Цвет" descFilter={colorArr} />
-    </>
-  );
+	return (
+		<>
+			<Stack
+				component="section"
+				direction="column"
+				alignItems="center"
+				sx={{
+					background: `linear-gradient(0deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.45) 100%), url(${headerBg.src}), lightgray 50% / cover no-repeat`,
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+				}}
+			>
+				<Typography
+					pt="127px"
+					variant="h1"
+					className={proheroFont.className}
+					color="#fff"
+					fontSize={55}
+					letterSpacing={5.5}
+					width={560}
+				>
+					Ride bikes. Have fun. Feel good.
+				</Typography>
+				<ButtonUi
+					margin="65px 0 285px"
+					text="Найди свой велосипед"
+					color="#fff"
+					border="2px solid #fff"
+				/>
+			</Stack>
+			<Filter nameFilter="Цвет" descFilter={colorArr} />
+		</>
+	);
 }
 
 export default HeroSection;
